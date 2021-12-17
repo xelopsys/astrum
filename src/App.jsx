2b4fd3375e72ed96417df90aa56bdfc21b616f70
xelopsys <juraqulovjamshid7@gmail.com>
2021-12-18 01:13:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./components/Header"
+
+function App() {
+  return (
+        <div>
+        <Header/>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    plugins: [],
+    // ...
+}
