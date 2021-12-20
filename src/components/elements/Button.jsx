1966dfcd9b55d1efe87f20dc5562627b7008ca4c
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2021-12-20 16:49:36 +0500

    update

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Button = ({ value, href }) => {
   return (
-    <Link to="/">
+    <Link to={href}>
       <button className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
         {value}
       </button>
