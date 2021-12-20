8172293caaa7125e5b8eabfb55b3733df208b897
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2021-12-20 16:40:21 +0500

    Structuring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,7 @@ import {
   XIcon,
 } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
+import Button from "./elements/Button";
 
 const solutions = [
   {
@@ -308,12 +309,7 @@ export default function Header() {
             >
               Sign in
             </a>
-            <a
-              href="/"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            >
-              Sign up
-            </a>
+            <Button value="Sign up" href="/signup" />
           </div>
         </div>
       </div>
---
+++ src/components/elements/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Button = ({ value, href }) => {
+  return (
+    <Link to="/">
+      <button className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+        {value}
+      </button>
+    </Link>
+  );
+};
+
+export default Button;
